f8dc7d752de5e402b9a02e37e71d81db9ba6911f
Alithewebdesigner <82018535+Alithewebdesigner@users.noreply.github.com>
2021-07-13 11:11:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect("mongodb+srv://admin:admin@terminal.rwk1z.mongodb.net/test", {
+  .connect("mongodb+srv://admin:admin@terminal.rwk1z.mongodb.net/Terminal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
